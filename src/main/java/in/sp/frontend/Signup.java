d31e331c9b5681bee3daf3ca5a94293e03f8f87a
Aranjan0212 <ashishranjan02122001@gmail.com>
2023-12-08 00:42:19 +0530

    updated java file name

--- src/main/java/in/sp/frontend/Signup.java
+++ src/main/java/in/sp/frontend/Signup.java
@@ -10,7 +10,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-@WebServlet("/Signup.jsp")
+@WebServlet("/Signup")
 public class Signup extends HttpServlet
 {
 	
